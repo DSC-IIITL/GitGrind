02a6f6150ab32db9a61fcd94f0c24dbbd4552604
Prashant <prashantyadav09783@gmail.com>
2023-10-20 16:37:33 +0530

    Fixed the Bug in Next_Element

---
+++ Students/LCI2023025/Fix_Bugs/Next_Element.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> nextGreaterElements(vector<int>& nums) {
+    int n = nums.size();
+    nums.resize(2 * n);
+    vector<int> res(n, -1);
+    stack<int> st;
+
+    for (int i = 0; i < 2 * n; i++) {
+      nums[i] = nums[i - n];
+      while (!st.empty() && nums[i % n] > nums[st.top()]) { 
+        int idx =st.top();
+        st.pop();
+        res[idx] = nums[i % n];
+      }
+      st.push(i % n);
+    }
+    return res;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the circular array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements separated by spaces: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    vector<int> result = nextGreaterElements(nums);
+
+    cout << "Next Greater Elements in Circular Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+// There are few Logical errors in the code. Fix them and submit the code.
