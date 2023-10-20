e12fef582d6bbcbe1b7e5e17e6b82891f017ad32
digvijay2406 <digvijaydeshmukh5555@gmail.com>
2023-10-20 16:19:45 +0530

    Added a more optimized solution to sorting.cpp #35

--- Students/LCI2023012/Optimize_solution/sorting.cpp
+++ Students/LCI2023012/Optimize_solution/sorting.cpp
@@ -1,16 +1,33 @@
-#include <bits/stdc++.h>
+ #include <bits/stdc++.h>
 using namespace std;
+ 
+void Sort(int a[], int n)
+{
+    int i, k, j;
+    for (i = 1; i < n; i++) {
+        k = a[i];
+        j = i - 1;
 
-int main(){
+        while (j >= 0 && a[j] > k) {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = k;
+    }
+}
+int main()
+{
     int n;
     cin>>n;
     int a[n];
     for(int i = 0; i<n ; i++){
         cin>>a[i];
     }
-    sort(a,a+n);
-    cout<<"Sorted Array : ";
+    Sort(a, n);
+    
     for(int i = 0; i<n; i++){
         cout<<a[i]<<" ";
     }
+ 
+    return 0;
 }
