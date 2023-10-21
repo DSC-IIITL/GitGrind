e3c9134c43d217ed5d5b93f0923e0dd3f20049f1
Priyanshu-ryu <pr.agarwal2005@gmail.com>
2023-10-21 23:34:18 +0530

    Fixed Logic for Paired Char Problem
    Fixed the code for Paired Char problem to count the number of unique
    pairs of characters at even indexes with opposite cases.

--- Students/LIT2023022/Fix_Bugs/Paired_Char.cpp
+++ Students/LIT2023022/Fix_Bugs/Paired_Char.cpp
@@ -3,24 +3,17 @@ using namespace std;
 
 int countPairedCharacters(int n, string s)  
 {
-	int arr[256]={0};
-	int c = 0;
-	int cnt=0;
+	int freq[256]={0};
+	int cnt = 0;
 	for (int i = 0; i < 2*n; i=i+2)       
-	{   
-		if (arr[s[i]]==0){
-			cnt=cnt+1;
-		}
-        arr[s[i]]++; 		
-					
+	{
+        freq[s[i]]++;                             // updating frequency of charcters
 	}
 
-    for (int i=65;i<126; i++) {
-        if (arr[i]>=1 && arr[i+32]>=1){
-            c=c+1;
-        }
+    for (int i=0;i<26; i++) {               
+        cnt=cnt+min(freq[65+i],freq[97+i]);       //to include count of pair exactly once 
     } 
-	return c;
+	return cnt;
 }
 
 
